575804013cf0646652adaf0d74eb2bd50e9a50b4
julifgo <juli_fgo@hotmail.com>
2015-04-20 00:59:08 -0300

    Update main.cpp
    ejemplo de uso de funciones

--- TPLAB/TPLAB/menu/main.cpp
+++ TPLAB/TPLAB/menu/main.cpp
@@ -10,17 +10,24 @@ int main() {
 	int pausa;
 
 	CorrePocoyo<int> carrera;
+	if(carrera.esVacia())
+		cout<<"La carrera todavia esta vacia"<<endl;
 	carrera.nuevoCorredor(1);
 	carrera.nuevoCorredor(2);
 	carrera.nuevoCorredor(3);
 	cout << "Tamanio Carrera: " << carrera.tamanio() << endl;
 	cout << "Primero: " << carrera.damePrimero() << endl;
+	cout<<carrera<<endl;
 	carrera.nuevoCorredor(4,1);
+	cout<<carrera<<endl;
 	cout << "Primero: " << carrera.damePrimero() << endl;
 	carrera.sobrepasar(2);
+	cout<<carrera<<endl;
 	carrera.sobrepasar(2);
+	cout<<carrera<<endl;
 	cout << "Primero: " << carrera.damePrimero() << endl;
 	carrera.sobrepasar(4);
+	cout<<carrera<<endl;
 	cout << "Primero: " << carrera.damePrimero() << endl;
 	cout << carrera.dameCorredorEnPos(1) << carrera.dameCorredorEnPos(2) << carrera.dameCorredorEnPos(3) << carrera.dameCorredorEnPos(4) << endl;
 	cout << "Filmado: " << carrera.corredorFilmado() << endl;
